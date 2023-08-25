7de08ca91c5d9ce9b1a0beb0fadbbccf34aacecf
DmitriyShkredov <dmitriyshkredovdev@gmail.com>
2023-08-25 18:51:57 +0300

    feat: add file to base64

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const [data, setData] = useState("");
+
+  const onChange = (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.onloadend = () => setData(reader.result);
+    reader.readAsDataURL(file);
+  };
+
+  return (
+    <div className="App">
+      <input type="file" onChange={onChange} />
+      <img src={data} alt="" />
+      <p>{data}</p>
+    </div>
+  );
 }
 
 export default App;
